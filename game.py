b799a83eef8254595b8955e4ef51e794ee06886d
nkhatri9 <nikitameeta@yahoo.com>
2022-01-31 17:38:34 -0500

    Update game.py

--- game.py
+++ game.py
@@ -5,3 +5,63 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+import random
+import os
+
+def determine_winner(userMove, computerMove):
+    #user chooses rock
+    gameResult = None
+    if userMove == "rock" and computerMove == "paper":
+        print("You lose!")
+    elif userMove == "rock" and computerMove == "scissors":
+        print("You win!")
+    elif userMove == "rock" and computerMove == "rock":
+        print("It's a tie!")
+
+    #user chooses paper
+
+    if userMove == "paper" and computerMove == "scissors":
+        print("You lose!")
+    elif userMove == "paper" and computerMove == "rock":
+        print("You win!")
+    elif userMove == "paper" and computerMove == "paper":
+        print("It's a tie!")
+
+    #user chooses scissors
+    if userMove == "scissors" and computerMove == "rock":
+        print("You lose!")
+    elif userMove == "scissors" and computerMove == "paper":
+        print("You win!")
+    elif userMove == "scissors" and computerMove == "scissors":
+        print("It's a tie!")
+    
+def main():
+    
+    #list of possible user moves
+    choiceList = ["rock", "paper", "scissors"]
+
+    #Welcome User to game
+    print("Welcome to my rock, paper, and scissors game!")
+
+    #Get user input convert to lower case
+    userChoice = None
+
+    #make sure user chooses valid input
+    while userChoice not in choiceList:
+        userChoice = input("Please type in rock, paper, or scissors: ").lower()
+        print()
+    
+    #Tell user what they chose
+    print("You chose", userChoice + "!")
+
+    #computer selects move
+    computerChoice = random.choice(choiceList)
+
+
+    print("The computer chose", computerChoice)
+
+    #determine who wins
+    determine_winner(userChoice, computerChoice)
+
+main()
